619aebf56293803826187f512fdeaf3eef28db50
Elliott Hebert <elliott.hebert.pro@gmail.com>
2018-02-19 20:02:51 +0100

    add tour section component

--- app/frontend/components/tour/index.js
+++ app/frontend/components/tour/index.js
@@ -1,3 +1,4 @@
 import "components/tour/title/tour_title";
 import "components/tour/description/tour_description";
 import "components/tour/call_to_action/tour_call_to_action";
+import "components/tour/section/tour_section";
