2f8ecf5d946e273c1fd4fbf325bc7f200516c70e
Elliott Hebert <elliott.hebert.pro@gmail.com>
2018-02-19 21:31:28 +0100

    create tour side component

--- app/frontend/components/tour/index.js
+++ app/frontend/components/tour/index.js
@@ -3,3 +3,4 @@ import "components/tour/description/tour_description";
 import "components/tour/call_to_action/tour_call_to_action";
 import "components/tour/section/tour_section";
 import "components/tour/award/tour_award";
+import "components/tour/side/tour_side";
