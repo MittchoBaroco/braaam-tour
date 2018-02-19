fc38d8ed7f3f73552828fa69ef48cc73dde8dda2
Elliott Hebert <elliott.hebert.pro@gmail.com>
2018-02-19 20:13:36 +0100

    add award component

--- app/frontend/components/tour/index.js
+++ app/frontend/components/tour/index.js
@@ -2,3 +2,4 @@ import "components/tour/title/tour_title";
 import "components/tour/description/tour_description";
 import "components/tour/call_to_action/tour_call_to_action";
 import "components/tour/section/tour_section";
+import "components/tour/award/tour_award";
