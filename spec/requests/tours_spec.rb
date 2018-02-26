cf20e48611ac1b247c2178c29cff757fd6bbbc36
Bill Tihen <btihen@gmail.com>
2018-02-26 17:16:17 +0100

    cleanup request tour_spec

--- spec/requests/tours_spec.rb
+++ spec/requests/tours_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Tours", type: :request do
   describe "GET /tours" do
-    skip("wait for it")
-    it "works! (now write some real specs)" do
+    it "tours#index" do
       get tours_path
       expect(response).to have_http_status(200)
     end
