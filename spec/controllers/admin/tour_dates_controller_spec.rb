e83d6e40b7bfe0a8d1d61f2e7dcf730e6fc0d4e5
Bill Tihen <btihen@gmail.com>
2018-03-03 11:27:31 +0100

    tour_date authentication - passes

--- spec/controllers/admin/tour_dates_controller_spec.rb
+++ spec/controllers/admin/tour_dates_controller_spec.rb
@@ -2,120 +2,161 @@ require 'rails_helper'
 
 RSpec.describe Admin::TourDatesController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Admin::TourDate. As you add validations to Admin::TourDate, be sure to
-  # adjust the attributes here as well.
+  include Devise::Test::ControllerHelpers
+
+  # attributes
   let!(:tour) {
     FactoryBot.create(:tour)
+  }
+  let!(:tour_date) {
+    FactoryBot.create(:tour_date)
+  }
+  let(:new_attributes) {
+    {day: Date.tomorrow}
+  }
+  let(:invalid_attributes) {
+    {day: "", tour: ""}
   }
   let(:valid_attributes) {
     FactoryBot.attributes_for(:tour_date, tour_id: tour.id)
   }
 
-  let(:invalid_attributes) {
-    {day: "", tour: ""}
-  }
+  # user & session - for security / authorization testing
+  let!(:manager) { FactoryBot.create(:manager) }
+  # let!(:valid_session) { {} }
+  let!(:valid_session) { {manager_id: manager.id} }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # Admin::TourDatesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "returns a success response" do
-      tour_date = TourDate.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
+  context "UNAUTHENTICATED" do
+    describe "redirect" do
+      it "from GET #index to login page" do
+        get :index
+        expect(response).to redirect_to( new_manager_session_path )
+      end
+      it "from GET #show to login page" do
+        get :show, params: {id: tour_date.to_param}
+        expect(response).to redirect_to( new_manager_session_path )
+      end
+      it "from GET #new to login page" do
+        get :new
+        expect(response).to redirect_to( new_manager_session_path )
+      end
+      it "from GET #show to login page" do
+        get :edit, params: {id: tour_date.to_param}
+        expect(response).to redirect_to( new_manager_session_path )
+      end
+      it "from POST #create to login page" do
+        post :create, params: {admin_tour: invalid_attributes}
+        expect(response).to redirect_to( new_manager_session_path )
+      end
+      it "from PUT #show to login page" do
+        put :update, params: {id: tour_date.to_param, admin_tour: new_attributes}
+        expect(response).to redirect_to( new_manager_session_path )
+      end
+      it "from PATCH #show to login page" do
+        patch :update, params: {id: tour_date.to_param, admin_tour: new_attributes}
+        expect(response).to redirect_to( new_manager_session_path )
+      end
+      it "from DELETE #destroy to login page" do
+        delete :destroy, params: {id: tour_date.to_param}
+        expect(response).to redirect_to( new_manager_session_path )
+      end
     end
   end
 
-  describe "GET #show" do
-    it "returns a success response" do
-      tour_date = TourDate.create! valid_attributes
-      get :show, params: {id: tour_date.to_param}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
+  context "AUTHENTICATED as manager" do
 
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_successful
+    # Devise Valid Session testing - requires more:
+    # https://github.com/plataformatec/devise/wiki/how-to:-stub-authentication-in-controller-specs
+    before(:each) do
+      sign_in manager
     end
-  end
 
-  describe "GET #edit" do
-    it "returns a success response" do
-      tour_date = TourDate.create! valid_attributes
-      get :edit, params: {id: tour_date.to_param}, session: valid_session
-      expect(response).to be_successful
+    describe "GET #index" do
+      it "returns a success response" do
+        # tour_date = TourDate.create! valid_attributes
+        get :index, params: {}, session: valid_session
+        expect(response).to be_successful
+      end
     end
-  end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Admin::TourDate" do
-        expect {
+    describe "GET #show" do
+      it "returns a success response" do
+        # tour_date = TourDate.create! valid_attributes
+        get :show, params: {id: tour_date.to_param}, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET #new" do
+      it "returns a success response" do
+        get :new, params: {}, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+    describe "GET #edit" do
+      it "returns a success response" do
+        # tour_date = TourDate.create! valid_attributes
+        get :edit, params: {id: tour_date.to_param}, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+
+    describe "POST #create" do
+      context "with valid params" do
+        it "creates a new Admin::TourDate" do
+          expect {
+            post :create, params: {admin_tour_date: valid_attributes}, session: valid_session
+          }.to change(TourDate, :count).by(1)
+        end
+        it "redirects to the created admin_tour_date" do
           post :create, params: {admin_tour_date: valid_attributes}, session: valid_session
-        }.to change(TourDate, :count).by(1)
+          expect(response).to redirect_to( admin_tour_date_path(TourDate.last) )
+        end
       end
-
-      it "redirects to the created admin_tour_date" do
-        post :create, params: {admin_tour_date: valid_attributes}, session: valid_session
-        expect(response).to redirect_to( admin_tour_date_path(TourDate.last) )
-      end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {admin_tour_date: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      tomorrow = Date.today + 1
-      let(:new_attributes) {
-        { day: tomorrow }
-      }
-
-      it "updates the requested admin_tour_date" do
-        tour_date = TourDate.create! valid_attributes
-        put :update, params: {id: tour_date.to_param, admin_tour_date: new_attributes}, session: valid_session
-        tour_date.reload
-        expect( tour_date.day ).to eq( tomorrow )
-      end
-
-      it "redirects to the admin_tour_date" do
-        tour_date = TourDate.create! valid_attributes
-        put :update, params: {id: tour_date.to_param, admin_tour_date: valid_attributes}, session: valid_session
-        expect(response).to redirect_to( admin_tour_date_path(tour_date) )
+      context "with invalid params" do
+        it "returns a success response (i.e. to display the 'new' template)" do
+          post :create, params: {admin_tour_date: invalid_attributes}, session: valid_session
+          expect(response).to be_successful
+        end
       end
     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
-        tour_date = TourDate.create! valid_attributes
-        put :update, params: {id: tour_date.to_param, admin_tour_date: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
+    describe "PUT #update" do
+      context "with valid params" do
+        it "updates the requested admin_tour_date" do
+          # tour_date = TourDate.create! valid_attributes
+          put :update, params: {id: tour_date.to_param, admin_tour_date: new_attributes}, session: valid_session
+          tour_date.reload
+          expect( tour_date.day ).to eq( Date.tomorrow )
+        end
+        it "redirects to the admin_tour_date" do
+          # tour_date = TourDate.create! valid_attributes
+          put :update, params: {id: tour_date.to_param, admin_tour_date: valid_attributes}, session: valid_session
+          expect(response).to redirect_to( admin_tour_date_path(tour_date) )
+        end
+      end
+      context "with invalid params" do
+        it "returns a success response (i.e. to display the 'edit' template)" do
+          # tour_date = TourDate.create! valid_attributes
+          put :update, params: {id: tour_date.to_param, admin_tour_date: invalid_attributes}, session: valid_session
+          expect(response).to be_successful
+        end
       end
     end
-  end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested admin_tour_date" do
-      tour_date = TourDate.create! valid_attributes
-      expect {
+    describe "DELETE #destroy" do
+      it "destroys the requested admin_tour_date" do
+        # tour_date = TourDate.create! valid_attributes
+        expect {
+          delete :destroy, params: {id: tour_date.to_param}, session: valid_session
+        }.to change(TourDate, :count).by(-1)
+      end
+      it "redirects to the admin_tour_dates list" do
+        # tour_date = TourDate.create! valid_attributes
         delete :destroy, params: {id: tour_date.to_param}, session: valid_session
-      }.to change(TourDate, :count).by(-1)
-    end
-
-    it "redirects to the admin_tour_dates list" do
-      tour_date = TourDate.create! valid_attributes
-      delete :destroy, params: {id: tour_date.to_param}, session: valid_session
-      expect(response).to redirect_to(admin_tour_dates_url)
+        expect(response).to redirect_to(admin_tour_dates_url)
+      end
     end
   end
 
