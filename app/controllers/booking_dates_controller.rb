1997eb6dfe84ed58e164571c711a51978a5a8e5e
Bill Tihen <btihen@gmail.com>
2018-03-24 19:48:06 +0100

    refuse booking when already booked & return to tour?

--- app/controllers/booking_dates_controller.rb
+++ app/controllers/booking_dates_controller.rb
@@ -12,23 +12,41 @@ class BookingDatesController < ApplicationController
     respond_to do |format|
       # TODO: add a company password check for commit?
       @tour = @booking_date.tour
+
+      # only process a new booking if no other company has already booked
+      unless @booking_date.company.blank?
+        format.html { redirect_to @tour,
+                                    alert: 'Sorry, date already booked.' }
+        format.json { render :show, status: :date_already_booked,
+                                    location: @tour }
+      end
+
+      # find the company and create booking params for saving
       company, new_params = BookingStrategy.new(params: get_booking_info,
                                                 action: :signup).run
+
+      # if the company can be found and booking date succesfully updates
       if company and @booking_date.update( new_params )
         # TODO: email company of sucessful booking signup
         format.html { redirect_to @tour,
                                   notice: 'Date was successfully booked.' }
         format.json { render :show, status: :ok, location: @tour }
-      elsif company.blank?
-        # format.html { render :edit }
+      end
+
+      # the company id or email couldn't be found in the list of partner companies
+      if company.blank?
         format.html { flash.now[:alert] = 'Company not found'; render :book }
         format.json { render json: @booking_date.errors,
                                   status: :unprocessable_entity }
-      else
-        format.html { flash.now[:alert] = 'Unexpected error'; render :book }
-        format.json { render json: @booking_date.errors,
-                                  status: :unprocessable_entity }
       end
+
+      # who knows what went wrong if we got here - log and report this problem
+      # TODO: configure rollbar (& get an account)
+      # Rollbar.error("Unknown Booking error -- BookingDate: #{@booking_date}, saving errors: #{@booking_date.errors.messages} - incomming params: #{booking_date_params}, processsed params #{new_params}", saving_errors: @booking_date.errors.messages)
+      Rails.logger.error "Unknown Booking error -- BookingDate: #{@booking_date}, saving errors: #{@booking_date.errors.messages}\n\t- incomming params: #{booking_date_params}, processsed params #{new_params}"
+      format.html { flash.now[:alert] = 'Unexpected error'; render :book }
+      format.json { render json: @booking_date.errors,
+                                  status: :unprocessable_entity }
     end
   end
 
