72b75a41a8aa36514ac3a875e45e2606f5bd6060
Elliott Hebert <elliott.hebert.pro@gmail.com>
2018-06-03 20:06:11 +0200

    fix calendar instansation

--- app/frontend/components/tour/datepicker/tour_datepicker.js
+++ app/frontend/components/tour/datepicker/tour_datepicker.js
@@ -8,30 +8,32 @@ import 'fullcalendar/dist/locale/en-gb.js';
 import "./tour_datepicker.css";
 
 
-$('#calendar').fullCalendar({
-  locale: getLocale(),
-  dayClick: function(date, jsEvent, view) {
-    if ($(this).css('background-color') != "rgb(61, 118, 175)") {
-      var random = Math.floor((Math.random() * 9999999999999) + 1000000000000);
-      $('<input>').attr({
-          type: 'hidden',
-          id: 'tour_booking_dates_attributes_'+ random +'_day',
-          name: 'tour[booking_dates_attributes]['+ random +'][day]',
-          value: date.format()
-      }).appendTo($('#calendar').data("target"));
-      $('<input>').attr({
-          type: 'hidden',
-          id: 'tour_booking_dates_attributes_'+ random +'_close',
-          name: 'tour[booking_dates_attributes]['+ random +'][close]',
-          value: 0
-      }).appendTo($('#calendar').data("target"));
-      $(this).css('background-color', '#3D76AF');
-      $(this).data('field', random);
-    } else {
-      $("input[id*='" + $(this).data('field') + "']").remove();
-      $(this).css('background-color', '#fff');
+$( document ).ready(function() {
+  $('#calendar').fullCalendar({
+    locale: getLocale(),
+    dayClick: function(date, jsEvent, view) {
+      if ($(this).css('background-color') != "rgb(61, 118, 175)") {
+        var random = Math.floor((Math.random() * 9999999999999) + 1000000000000);
+        $('<input>').attr({
+            type: 'hidden',
+            id: 'tour_booking_dates_attributes_'+ random +'_day',
+            name: 'tour[booking_dates_attributes]['+ random +'][day]',
+            value: date.format()
+        }).appendTo($('#calendar').data("target"));
+        $('<input>').attr({
+            type: 'hidden',
+            id: 'tour_booking_dates_attributes_'+ random +'_close',
+            name: 'tour[booking_dates_attributes]['+ random +'][close]',
+            value: 0
+        }).appendTo($('#calendar').data("target"));
+        $(this).css('background-color', '#3D76AF');
+        $(this).data('field', random);
+      } else {
+        $("input[id*='" + $(this).data('field') + "']").remove();
+        $(this).css('background-color', '#fff');
+      }
     }
-  }
+  });
 });
 
 function getLocale() {
