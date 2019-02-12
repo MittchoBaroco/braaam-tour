1554b1f5a20604f409074596f5c44b3dd0092326
Elliott Hébert <ell.heb@container4.ch>
2019-02-12 21:02:08 +0100

    fix dynamic dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,22 @@
 module ApplicationHelper
+  def season(date=Date.today)
+    return :summer if date >= Date.strptime("01-Apr-#{date.year.to_s[2..4]}", "%d-%b-%y") and
+                      date <= Date.strptime("31-Oct-#{date.year.to_s[2..4]}", "%d-%b-%y")
+
+    return :winter_b_ny if date >= Date.strptime("01-Nov-#{date.year.to_s[2..4]}", "%d-%b-%y") and
+                           date <= Date.strptime("31-Dec-#{(date.year + 1).to_s[2..4]}}", "%d-%b-%y")
+
+    return :winter_a_ny if date >= Date.strptime("01-Jan-#{date.year.to_s[2..4]}", "%d-%b-%y") and
+                           date <= Date.strptime("31-Mar-#{date.year.to_s[2..4]}", "%d-%b-%y")
+  end
+
   def summer_dates(date=Date.today)
-    return ["01-Apr-#{Date.today.year.to_s[2..4]}", "30-Oct-#{Date.today.year.to_s[2..4]}"] if season(date) == :summer
-    return ["01-Apr-#{(Date.today.year + 1).to_s[2..4]}", "30-Oct-#{(Date.today.year + 1).to_s[2..4]}"] if season(date) == :winter
+    return ["01-Apr-#{date.year.to_s[2..4]}", "31-Oct-#{date.year.to_s[2..4]}"] if season(date) == :summer or season(date) == :winter_a_ny
+    return ["01-Apr-#{(date.year + 1).to_s[2..4]}", "31-Oct-#{(date.year + 1).to_s[2..4]}"] if season(date) == :winter_b_ny
   end
 
   def winter_dates(date=Date.today)
-    return ["01-Nov-#{Date.today.year.to_s[2..4]}", "30-Mar-#{(Date.today.year + 1).to_s[2..4]}"]
-  end
-
-  def season(date=Date.today)
-    return :summer if date >= Date.strptime("01-Apr-#{Date.today.year.to_s[2..4]}", "%d-%b-%y") and date <= Date.strptime("30-Oct-#{Date.today.year.to_s[2..4]}", "%d-%b-%y")
-    return :winter if date >= Date.strptime("01-Nov-#{Date.today.year.to_s[2..4]}", "%d-%b-%y") and date <= Date.strptime("30-Mar-#{((Date.today.year) + 1).to_s[2..4]}", "%d-%b-%y")
+    return ["01-Nov-#{date.year.to_s[2..4]}", "31-Mar-#{(date.year + 1).to_s[2..4]}"] if season(date) == :summer or season(date) == :winter_b_ny
+    return ["01-Jan-#{date.year.to_s[2..4]}", "31-Mar-#{date.year.to_s[2..4]}"] if season(date) == :winter_a_ny
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,35 +11,71 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe ApplicationHelper, type: :helper do
-  it "return the correct summer dates when it's summer" do
-    summer_start = "01-Apr-#{Date.today.year.to_s[2..4]}"
-    summer_end = "30-Oct-#{Date.today.year.to_s[2..4]}"
-    expect(summer_dates("1-8-2018".to_date)).to eq([summer_start, summer_end])
+  it "returns the correct summer dates when it's summer" do
+    summer_start = "01-Apr-18"
+    summer_end = "31-Oct-18"
+    expect(summer_dates("2-Aug-2018".to_date)).to eq([summer_start, summer_end])
   end
 
-  it "return the correct winter dates when it's summer" do
-    winter_start = "01-Nov-#{Date.today.year.to_s[2..4]}"
-    winter_end = "30-Mar-#{(Date.today.year + 1).to_s[2..4]}"
-    expect(winter_dates("1-8-2018".to_date)).to eq([winter_start, winter_end])
+  it "returns the correct winter dates when it's summer" do
+    winter_start = "01-Nov-18"
+    winter_end = "31-Mar-19"
+    expect(winter_dates("2-Aug-2018".to_date)).to eq([winter_start, winter_end])
   end
 
-  it "return the correct winter dates when it's winter" do
-    winter_start = "01-Nov-#{Date.today.year.to_s[2..4]}"
-    winter_end = "30-Mar-#{(Date.today.year + 1).to_s[2..4]}"
-    expect(winter_dates("1-1-2019".to_date)).to eq([winter_start, winter_end])
+  it "returns the correct winter dates when it's winter before the new year" do
+    winter_start = "01-Nov-19"
+    winter_end = "31-Mar-20"
+    expect(winter_dates("1-Nov-2019".to_date)).to eq([winter_start, winter_end])
   end
 
-  it "return the correct summer dates when it's winter" do
-    summer_start = "01-Apr-#{(Date.today.year + 1).to_s[2..4]}"
-    summer_end = "30-Oct-#{(Date.today.year + 1).to_s[2..4]}"
-    expect(summer_dates("1-1-2019".to_date)).to eq([summer_start, summer_end])
+  it "returns the correct winter dates when it's winter after the new year" do
+    winter_start = "01-Jan-20"
+    winter_end = "31-Mar-20"
+    expect(winter_dates("1-Jan-2020".to_date)).to eq([winter_start, winter_end])
   end
 
-  it "returns the correct season when it's summer" do
-    expect(season("1-8-2018".to_date)).to eq(:summer)
+  it "returns the correct summer dates when it's winter before the new year" do
+    summer_start = "01-Apr-20"
+    summer_end = "31-Oct-20"
+    expect(summer_dates("1-Nov-2019".to_date)).to eq([summer_start, summer_end])
   end
 
-  it "returns the correct season when it's winter" do
-    expect(season("1-1-2019".to_date)).to eq(:winter)
+  it "returns the correct summer dates when it's winter after the new year" do
+    summer_start = "01-Apr-20"
+    summer_end = "31-Oct-20"
+    expect(summer_dates("1-Jan-2020".to_date)).to eq([summer_start, summer_end])
   end
+  #
+  # it "return the correct summer dates when it's summer" do
+  #   summer_start = "01-Apr-#{Date.today.year.to_s[2..4]}"
+  #   summer_end = "30-Oct-#{Date.today.year.to_s[2..4]}"
+  #   expect(summer_dates("2-Aug-2018".to_date)).to eq([summer_start, summer_end])
+  # end
+  #
+  # it "return the correct winter dates when it's summer" do
+  #   winter_start = "01-Nov-#{Date.today.year.to_s[2..4]}"
+  #   winter_end = "30-Mar-#{(Date.today.year + 1).to_s[2..4]}"
+  #   expect(winter_dates("2-Aug-2018".to_date)).to eq([winter_start, winter_end])
+  # end
+  #
+  # it "return the correct winter dates when it's winter" do
+  #   winter_start = "01-Nov-#{Date.today.year.to_s[2..4]}"
+  #   winter_end = "30-Mar-#{(Date.today.year + 1).to_s[2..4]}"
+  #   expect(winter_dates("1-Jan-2019".to_date)).to eq([winter_start, winter_end])
+  # end
+  #
+  # it "return the correct summer dates when it's winter" do
+  #   summer_start = "01-Apr-#{(Date.today.year + 1).to_s[2..4]}"
+  #   summer_end = "30-Oct-#{(Date.today.year + 1).to_s[2..4]}"
+  #   expect(summer_dates("1-Jan-2019".to_date)).to eq([summer_start, summer_end])
+  # end
+  #
+  # it "returns the correct season when it's summer" do
+  #   expect(season("1-Aug-2018".to_date)).to eq(:summer)
+  # end
+  #
+  # it "returns the correct season when it's winter" do
+  #   expect(season("1-Jan-2019".to_date)).to eq(:winter)
+  # end
 end
